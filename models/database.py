0248cbcf0f9f44d300219731b81d3e639568ab82
lehuy1121 <nguyenlehuy9000@gmail.com>
2024-11-11 14:19:39 +0700

    sevice sugguest friends

--- models/database.py
+++ models/database.py
@@ -1,12 +1,31 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-# Load biến môi trường từ file .env
+
+# Load environment variables from .env file
 load_dotenv()
-# Lấy MongoDB URI từ biến môi trường
-mongodb_uri = os.getenv("MONGODB_URI")
-# Kết nối với MongoDB
-client = MongoClient(mongodb_uri)
+
+# Get MongoDB credentials from environment variables
+MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_DATABASE = os.getenv("MONGO_DATABASE")
+MONGO_CLUSTER = os.getenv("MONGO_CLUSTER")
+
+# Check if the environment variables are correctly loaded
+print(MONGO_USERNAME, MONGO_PASSWORD, MONGO_DATABASE, MONGO_CLUSTER)
+
+# Construct the MongoDB connection URL
+if None in [MONGO_USERNAME, MONGO_PASSWORD, MONGO_DATABASE, MONGO_CLUSTER]:
+    raise ValueError("One or more MongoDB environment variables are missing.")
+
+mongo_url = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_CLUSTER}/{MONGO_DATABASE}?retryWrites=true&w=majority"
+
+# Connect to the MongoDB cluster
+client = MongoClient(mongo_url)
+
+# Connect to the MongoDB cluster
+client = MongoClient(mongo_url)
+
 db = client['socialdb']
 embeddings_collection = db['embeddings']
 search_results_collection = db['search_results']
